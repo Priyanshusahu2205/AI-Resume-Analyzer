c80891cbfcf90707b0e6bd26780c1657d09f23a6
Priyanshu sahu <priyanshusahu2205@gmail.com>
2026-05-19 19:16:26 +0530

    Added gunicorn

--- model/database.py
+++ model/database.py
@@ -3,8 +3,19 @@ from sqlalchemy import create_engine
 username = "root"
 password = "priyanshu%402205"
 
-DATABASE_URL = f"mysql+pymysql://{username}:{password}@localhost/resume_analyzer"
+DATABASE_URL = "sqlite:///resume.db"
 
 engine = create_engine(DATABASE_URL)
+
+with engine.connect() as connection:
+    connection.execute(text("""
+        CREATE TABLE IF NOT EXISTS resumes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            resume_text TEXT,
+            predicted_role TEXT,
+            score REAL
+        )
+    """))
+    connection.commit()
 
 print("Database Connected Successfully")
